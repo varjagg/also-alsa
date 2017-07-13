6fcafe8a79e7473dd692acadac8c6528a20949d4
Eugene Zaikonnikov <eugene@funcall.org>
2017-07-13 19:07:33 +0200

    exports

--- package.lisp
+++ package.lisp
@@ -2,5 +2,6 @@
 
 (defpackage #:also-alsa
   (:use #:cl #:cffi)
-  (:export #:pcm-stream #:alsa-open #:alsa-close #:ref #:alsa-write #:alsa-read))
+  (:export #:pcm-stream #:alsa-open #:alsa-close #:ref #:alsa-write #:alsa-read
+	   #:buffer #:direction #:element-type #:pcm-format #:channel-count #:buffer-size))
 
