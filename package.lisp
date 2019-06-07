c1a0c389e5d20e8a1a201d1c141ab6e8e7711be8
Eugene Zaikonnikov <eugene.zaikonnikov@norphonic.com>
2019-06-07 13:59:17 +0200

    export new methods

--- package.lisp
+++ package.lisp
@@ -4,5 +4,6 @@
   (:use #:cl #:cffi)
   (:export #:pcm-stream #:alsa-open #:alsa-close #:ref #:alsa-write #:alsa-read
 	   #:buffer #:direction #:element-type #:pcm-format #:channel-count #:buffer-size
-	   #:alsa-element-type #:contents-to-lisp #:with-alsa-device #:alsa-reopen))
+	   #:alsa-element-type #:contents-to-lisp #:with-alsa-device #:alsa-reopen
+	   #:get-delay #:drain))
 
