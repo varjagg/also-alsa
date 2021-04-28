3b413260ebd8ee21cbeaa7199c395ed3bc818691
Eugene Zaikonnikov <eugene@funcall.org>
2021-04-28 23:00:28 +0200

    add more of simple mixer api

--- also-mixer.lisp
+++ also-mixer.lisp
@@ -5,6 +5,9 @@
 (defcfun "snd_mixer_selem_register" :int (handle :pointer) (options :pointer) (classp :pointer))
 (defcfun "snd_mixer_load" :int (handle :pointer))
 ;; a macro… (defcfun "snd_mixer_selem_id_alloca" :int (handle :pointer))
+(defcfun "snd_mixer_selem_id_set_index" :void (sid :pointer) (val :uint))
+(defcfun "snd_mixer_selem_id_set_name" :void (sid :pointer) (val :string))
+(defcfun "snd_mixer_find_selem" :pointer (handle :pointer) (sid :pointer))
 (defcfun "snd_mixer_close" :int (handle :pointer))
 
 (defun set-master-volume (volume)
@@ -20,9 +23,9 @@
     (snd-mixer-selem-register handle (null-pointer) (null-pointer))
     (snd-mixer-load (deref handle))
     (snd-mixer-selem-id-alloca sid)
-    (snd-mixer-selem-id-set-index sid 0)
-    (snd-mixer-selem-id-set-name sid selem-name)
-    (setf selem (snd-mixer-find-selem handle sid))
+    (snd-mixer-selem-id-set-index (deref sid) 0)
+    (snd-mixer-selem-id-set-name (deref sid) selem-name)
+    (setf selem (snd-mixer-find-selem (deref handle) (deref sid)))
 
     (snd-mixer-selem-get-playback-volume-range selem min max)
     (snd-mixer-selem-set-playback-volume-all selem (round (* volume (/ (deref max) 100))))
