6995247bd7258439f6ab716a93f1a5f6e5e4b290
Eugene Zaikonnikov <eugene.zaikonnikov@norphonic.com>
2021-10-06 11:23:04 +0200

    export

--- package.lisp
+++ package.lisp
@@ -6,5 +6,5 @@
 	   #:buffer #:direction #:element-type #:pcm-format #:channel-count #:buffer-size
 	   #:alsa-element-type #:contents-to-lisp #:with-alsa-device #:alsa-reopen
 	   #:get-delay #:get-avail-delay #:drain #:alsa-wait #:alsa-start
-	   #:make-alsa-buffer #:alsa-resume #:*alsa-warn* #:alsa-warn))
+	   #:make-alsa-buffer #:alsa-resume #:*alsa-warn* #:alsa-warn #:get-state))
 
