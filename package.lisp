a8e4b1b3a080547b133f2c0ac68ee9801cca5fef
Eugene Zaikonnikov <ez@norphonic.com>
2023-07-07 21:49:58 +0200

    exports

--- package.lisp
+++ package.lisp
@@ -6,5 +6,9 @@
 	   #:buffer #:direction #:element-type #:pcm-format #:channel-count #:buffer-size
 	   #:alsa-element-type #:contents-to-lisp #:with-alsa-device #:alsa-reopen
 	   #:get-delay #:get-avail-delay #:drain #:alsa-wait #:alsa-start
-	   #:make-alsa-buffer #:alsa-resume #:*alsa-warn* #:alsa-warn #:get-state))
+	   #:make-alsa-buffer #:alsa-resume #:*alsa-warn* #:alsa-warn #:get-state
+	   #:open-mixer #:close-mixer #:access-mixer-element
+	   #:set-mixer-element-playback-volume #:set-mixer-element-playback-db
+	   #:set-mixer-element-capture-volume #:set-mixer-element-capture-db
+	   #:set-master-volume))
 
