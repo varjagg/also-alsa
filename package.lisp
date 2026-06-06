3be158bd6fe7f0e46d8960e17f0aae179f80ffd7
Eugene Zaikonnikov <eugene@funcall.org>
2026-06-06 22:07:10 +0200

    fix channels-count export

--- package.lisp
+++ package.lisp
@@ -3,7 +3,7 @@
 (defpackage #:also-alsa
   (:use #:cl #:cffi)
   (:export #:pcm-stream #:alsa-open #:alsa-close #:ref #:alsa-write #:alsa-read
-	   #:buffer #:direction #:element-type #:pcm-format #:channel-count #:buffer-size
+	   #:buffer #:direction #:element-type #:pcm-format #:channels-count #:buffer-size
 	   #:alsa-element-type #:contents-to-lisp #:with-alsa-device #:alsa-reopen
 	   #:get-delay #:get-avail-delay #:drain #:alsa-wait #:alsa-start
 	   #:make-alsa-buffer #:alsa-resume #:*alsa-warn* #:alsa-warn #:get-state
